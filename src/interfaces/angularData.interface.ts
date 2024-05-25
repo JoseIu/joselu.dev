21c1b7571fc43b8a34e97fa21c453e97601035c3
Joselu <joseluissgz97@gmail.com>
2024-05-25 22:55:03 +0200

    add: agularData Interface

---
+++ src/interfaces/angularData.interface.ts
@@ -0,0 +1,10 @@
+export interface AngularDataInterface {
+	id: string;
+	img: string;
+	title: string;
+	tegnologies: string[];
+	description: string;
+	features?: string[];
+	demo: string;
+	repo: string;
+}
